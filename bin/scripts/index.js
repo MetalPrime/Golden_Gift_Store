4405bc26d3642cf92269867bf306e5125c0e35b2
Jhon Garcia <metalprime57@gmail.com>
2020-09-22 02:04:09 -0500

    Update index.js

--- bin/scripts/index.js
+++ bin/scripts/index.js
@@ -5,14 +5,20 @@ const img = document.querySelectorAll('.experiences__interaction img');
 
 console.log(slider);
 console.log(img);
+
 function handleSlider () {
   console.log('hola', slider.value);
-  img.array.forEach(ele => {
-    ele.style.opacity = element.value;
-  });
-  
+ 
   //img1.style.width = (slider1.value * 100) + '%';
 }
+
 slider.forEach(element => {
-    element.addEventListener('input', handleSlider);
+    element.addEventListener('input', () =>{
+        img.forEach(ele => {
+            if(element === ele){
+                ele.style.opacity = element.value;
+            }
+            
+          }); 
+    });
 });
